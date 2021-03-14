5f58340aab6d3daf06045a4102993911d41c4aa4
Anuj Chavan <44212979+anujchavan112@users.noreply.github.com>
2021-03-14 19:37:18 +0530

    ch(app): add application files

---
+++ contentapi/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from .views import  RegisterAPI, ContentViewSet #LoginViewSet,
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register('content', ContentViewSet, basename='content')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', obtain_auth_token, name='login'),
+    
+]
